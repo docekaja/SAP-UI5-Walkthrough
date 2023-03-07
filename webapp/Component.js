409e4c83fa14f766d73c69486421af097a199f63
Jan Docekal <docekal26@gmail.com>
2023-03-07 16:53:04 +0100

    Step 10 - Descriptor for applications

--- webapp/Component.js
+++ webapp/Component.js
@@ -9,12 +9,7 @@ sap.ui.define(
     return UIComponent.extend('sap.ui.demo.walkthrough.Component', {
       metadata: {
         interfaces: ['sap.ui.core.IAsyncContentCreation'],
-        rootView: {
-          viewName: 'sap.ui.demo.walkthrough.view.App',
-          type: 'XML',
-          /*"async": true, // implicitly set via the sap.ui.core.IAsyncContentCreation interface*/
-          id: 'app',
-        },
+        manifest: 'json',
       },
       init: function () {
         // call the init function of the parent
@@ -27,12 +22,6 @@ sap.ui.define(
         };
         var oModel = new JSONModel(oData);
         this.setModel(oModel);
-
-        // set i18n model
-        var i18nModel = new ResourceModel({
-          bundleName: 'sap.ui.demo.walkthrough.i18n.i18n',
-        });
-        this.setModel(i18nModel, 'i18n');
       },
     });
   }
